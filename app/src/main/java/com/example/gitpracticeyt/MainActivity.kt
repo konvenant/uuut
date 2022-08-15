e288b8898b037a35b1205cf200129d0d07869d2a
KONVENANT <sundaycovenant4@gmail.com>
2022-08-15 14:34:58 -0700

    youtube commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 println("THIS IS MY CONTAINER")
-        println("experiment")
+        println("youTube")
+
     }
 }
