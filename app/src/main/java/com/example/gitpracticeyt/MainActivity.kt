02876f4c42497a1c7aeaef9797244e668a00250d
KONVENANT <sundaycovenant4@gmail.com>
2022-08-15 13:52:58 -0700

    last fuk up

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 println("THIS IS MY CONTAINER")
-        thfsfhf is lie
+
+        today is a good dafdy
     }
 }
