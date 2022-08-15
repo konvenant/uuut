b852fab46f4d73522b83fd4292d80d64a26072cd
KONVENANT <sundaycovenant4@gmail.com>
2022-08-15 13:53:15 -0700

    Revert "last fuk up"
    This reverts commit 02876f4c42497a1c7aeaef9797244e668a00250d.

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 println("THIS IS MY CONTAINER")
-
-        today is a good dafdy
+        thfsfhf is lie
     }
 }
